52e6a88d729d8f06b200315a5c1f72758e12b225
gutiantian123Abc <william.guxiangtian@gmail.com>
2020-06-03 16:18:22 -0400

    Remove Outermost Parentheses

---
+++ easy/Remove_Outermost_Parentheses.py
@@ -0,0 +1,78 @@
+'''
+1021. Remove Outermost Parentheses
+
+https://leetcode.com/problems/remove-outermost-parentheses/
+
+A valid parentheses string is either empty (""), "(" + A + ")", or A + B, 
+where A and B are valid parentheses strings, and + represents string concatenation.  
+For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+
+A valid parentheses string S is primitive if it is nonempty, 
+and there does not exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.
+
+Given a valid parentheses string S, consider its primitive decomposition: S = P_1 + P_2 + ... + P_k, 
+where P_i are primitive valid parentheses strings.
+
+Return S after removing the outermost parentheses of every primitive string in the primitive decomposition of S.
+
+ 
+
+Example 1:
+
+Input: "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+Example 3:
+
+Input: "()()"
+Output: ""
+Explanation: 
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+ 
+
+Note:
+
+S.length <= 10000
+S[i] is "(" or ")"
+S is a valid parentheses string
+
+'''
+
+class Solution(object):
+    def removeOuterParentheses(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        left = 0
+        marks = []
+        
+        for i in range(len(S)):
+            c = S[i]
+            if c == '(':
+                if left == 0:
+                    marks.append(i)
+                left += 1
+            else:
+                left -= 1
+                if left == 0:
+                    marks.append(i)
+        res = ""
+        
+        for i in range(len(S)):
+            c = S[i]
+            if i not in marks:
+                res += c
+            
+        return res
+        
